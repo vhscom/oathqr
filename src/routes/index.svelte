f2597eebd73f17596ac930dcd5be4ffb078311fb
vhs <vhsdev@tutanota.com>
2022-02-10 15:56:23 +0800

    fix: 🐛 remove inline style rule
    close: #2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,20 @@
-<script>
+<script lang="ts">
 	import About from '$lib/About.svelte';
 	import Form from '$lib/Form.svelte';
 	import QR from 'svelte-qr';
 	import Example from '$lib/Example.svelte';
 	import Footer from '$lib/Footer.svelte';
+	import { beforeUpdate } from 'svelte';
 
 	let size = 200;
 	let text = 'otpauth://totp/?secret=';
+	let container: HTMLDivElement;
+	beforeUpdate(() => {
+		if (container) {
+			container.style.width = `${size}px`;
+			container.style.height = `${size}px`;
+		}
+	});
 </script>
 
 <header class="mt-4 flex flex-col items-center">
@@ -23,11 +31,8 @@
 </section>
 
 <section class="my-10 flex flex-col space-y-12">
-	<div
-		class="block self-center bg-gray-50 p-4 shadow-inner dark:bg-gray-200"
-		style="width:{size}px"
-	>
-		{#key `${text}-${size}`}
+	<div bind:this={container} class="block self-center bg-gray-50 p-4 shadow-inner dark:bg-gray-200">
+		{#key text}
 			<QR {text} />
 		{/key}
 	</div>
