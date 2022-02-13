68188c640a24446ea2435a3f33ca35c07e50b4a9
vhs <vhsdev@tutanota.com>
2022-02-13 19:37:39 +0800

    refactor: ♻️ portray example more accurately

--- src/lib/Example.svelte
+++ src/lib/Example.svelte
@@ -24,19 +24,19 @@
 	>
 	to see something like:
 </p>
-<div class="border-y-8 border-gray-200 dark:border-black dark:bg-gray-900">
-	<div class="flex items-center px-4">
+<div class="border-y-8 border-gray-200 dark:border-black/20 dark:bg-gray-900/20">
+	<div class="flex items-center pt-4 px-6">
 		<div
-			class="flex h-12 w-12 items-center justify-center rounded-full bg-gray-600 text-2xl font-light text-white"
+			class="flex h-16 w-16 items-center justify-center rounded-full bg-gray-600/60 text-3xl font-light text-white dark:text-gray-300"
 		>
 			H
 		</div>
 		<div class="flex flex-col p-4">
 			<div>
-				<span class="font-semibold">hub.docker.com</span>
+				<span class="font-semibold dark:text-gray-300">hub.docker.com</span>
 				<span class="text-gray-400 dark:text-gray-500">(vhsdev)</span>
 			</div>
-			<span class="text-3xl font-semibold text-[#388fed]">{code}</span>
+			<span class="text-3xl font-semibold text-blue-600 dark:text-gray-300">{code}</span>
 		</div>
 	</div>
 	<progress aria-hidden="true" bind:this={progress} class="-mb-2 h-1 w-full" max="100" value="70" />
