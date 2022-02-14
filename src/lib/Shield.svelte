448a4feb4de8070c8d6cfecc475f5b2951dd9efc
vhs <vhsdev@tutanota.com>
2022-02-14 23:51:09 +0800

    wip

---
+++ src/lib/Shield.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { onInterval } from './utils';
+
+	let shield: HTMLCanvasElement;
+	let ctx: WebGLRenderingContextBase;
+	onMount(() => {
+		// return;
+		ctx = shield.getContext('webgl');
+		if (ctx === null) return;
+		// Set clear color to black, fully transparent
+		ctx.clearColor(0.0, 0.0, 0.0, 0.0);
+		// Clear the color buffer with specified clear color
+		ctx.clear(ctx.COLOR_BUFFER_BIT);
+		// Clear the color at 60fps
+		// onInterval(() => {
+		// 	ctx.clear(ctx.COLOR_BUFFER_BIT);
+		// }, 1000 / 60 /* 60fps */);
+		console.log(ctx);
+	});
+</script>
+
+<canvas
+	bind:this={shield}
+	class="absolute top-0 left-0 p-3.5 z-10 w-full h-full filter backdrop-blur-sm pointer-events-none"
+/>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@
 	import Example from '$lib/Example.svelte';
 	import Footer from '$lib/Footer.svelte';
 	import Heading from '$lib/Heading.svelte';
-
+	import Shield from '$lib/Shield.svelte';
 	let size = 200;
 	let text = 'otpauth://totp/?secret=';
 
@@ -33,11 +33,14 @@
 	<div
 		id="qr-container"
 		bind:this={container}
-		class="block self-center bg-gray-100 p-4 shadow-inner dark:bg-oath-50/20 dark:shadow-oath-900"
+		class="relative self-center bg-gray-100 p-4 shadow-inner dark:bg-oath-50/20 dark:shadow-oath-900"
 	>
-		{#key text}
-			<QR {text} />
-		{/key}
+		<Shield />
+		<div class="absolute top-0 left-0 w-full">
+			{#key text}
+				<QR {text} />
+			{/key}
+		</div>
 	</div>
 </section>
 
