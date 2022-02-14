2842132df72deb2dd843e39ccbb15a34c20b2e58
vhs <vhsdev@tutanota.com>
2022-02-14 11:49:08 +0800

    refactor: ♻️ remove extra wrapper div

--- src/lib/Instructions.svelte
+++ src/lib/Instructions.svelte
@@ -1,11 +1,9 @@
-<div>
-	<p class="text-sm">
-		<b>Usage instructions:</b> Build from source (link below), and generate and scan your QR code
-		once you've reviewed the code for potential vulnerabilities. OATHqr uses a strict
-		<a
-			rel="external noopener"
-			tabindex="-1"
-			href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP">Content-Security Policy</a
-		> to ensure conforming browsers cannot communicate outside the loopback interface.
-	</p>
-</div>
+<p class="text-sm">
+	<b>Usage instructions:</b> Build from source (link below), and generate and scan your QR code once
+	you've reviewed the code for potential vulnerabilities. OATHqr uses a strict
+	<a
+		rel="external noopener"
+		tabindex="-1"
+		href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP">Content-Security Policy</a
+	> to ensure conforming browsers cannot communicate outside the loopback interface.
+</p>
