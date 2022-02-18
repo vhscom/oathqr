41fdbb39febed77906a8b876af0d9ed0c5a84a0a
vhs <vhsdev@tutanota.com>
2022-02-18 17:54:37 +0800

    chore: 🔧 add open graph meta

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,15 @@
 <svelte:head>
 	<meta name="license" href="https://www.gnu.org/licenses/agpl-3.0.en.html" />
+	<meta property="og:title" content="OATHqr" />
+	<meta property="og:description" content="Turn your secrets into scannable QR codes." />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://oathqr.vercel.app/" />
+	<meta property="og:image" content="https://vhs.codeberg.page/code/oathqr/oathqr.png" />
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:creator" content="@vhsdev" />
+	<meta property="twitter:title" content="OATHqr" />
+	<meta property="twitter:description" content="Turn your secrets into scannable QR codes." />
+	<meta property="twitter:image" content="https://vhs.codeberg.page/code/oathqr/oathqr.png" />
 </svelte:head>
 
 <ul class="list-none flex-col self-center text-center sm:flex-row">
